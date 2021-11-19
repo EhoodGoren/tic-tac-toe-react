1d995a906827abe4f7cd1509a62ac83543f502c1
EhoodGoren <ehoodgoren@gmail.com>
2021-11-19 16:17:44 +0200

    Created basic board for the game.

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Game from './Game';
+import './styles.css';
 
 
 ReactDOM.render(
   <React.StrictMode>
-
+    <Game />
   </React.StrictMode>,
   document.getElementById('root')
 );
